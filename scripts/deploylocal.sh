7ed11b8569d996995655d70ddb31dd548d7915c6
Jowen <jowen.xiao@viewfin.com>
2018-08-02 10:30:30 +0800

    update deploylocal.sh for re-deploy

--- scripts/deploylocal.sh
+++ scripts/deploylocal.sh
@@ -6,12 +6,16 @@ if [ ! -e "$SOURCE_DIR" ]; then
     cd "${PROJECT_DIR}/src"
     echo "clone ScanService.git ..."
     git clone https://gitee.com/metaverse/ScanService.git
+else
+    cd "$SOURCE_DIR"
+    git pull origin master
 fi
 
 for i in "$@"; do
 
 TARGET_PATH="${PROJECT_DIR}/$i"
 echo "deploy $i to ${TARGET_PATH}";
+rm -rf ${TARGET_PATH}
 mkdir -p ${TARGET_PATH}
 cp -r "$SOURCE_DIR" "$TARGET_PATH"
 
