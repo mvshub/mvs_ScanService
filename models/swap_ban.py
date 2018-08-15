1d0829bb74c33e81396a5b7df320948f408abd02
lxffff1990 <lxffff1990@163.com>
2018-08-15 15:20:45 +0800

    update db name

--- models/swap_ban.py
+++ models/swap_ban.py
@@ -3,7 +3,7 @@ import time
 
 
 class Swap_ban(db.Model):
-    __tablename__ = 'swap'
+    __tablename__ = 'Swap_ban'
 
     iden = db.Column(db.Integer, primary_key=True, autoincrement=True)
     swap_address = db.Column(db.String(128))
