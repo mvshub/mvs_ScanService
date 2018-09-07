edd8e5da0f573f56d9e0c273d72e4d6169f839c5
Jowen <jowen.xiao@viewfin.com>
2018-09-07 10:49:28 +0800

    add ETH scan config

--- rpcs/eth.py
+++ rpcs/eth.py
@@ -12,9 +12,10 @@ from models import constants
 
 class Eth(Base):
 
-    def __init__(self, settings):
+    def __init__(self, settings, tokens):
         Base.__init__(self, settings)
         self.name = 'ETH' if settings.get('name') is None else settings['name']
+        self.tokens = tokens
         self.contract_mapaddress = settings['contract_mapaddress'].lower()
 
         self.tx_verify_uri = settings['tx_verify_uri']
